203d09b83f66969a4964ebda19619955c899f37e
Omkar Sutar <159997804+OMKARSUTAR09@users.noreply.github.com>
2024-07-22 16:05:30 +0530

    Add files via upload
    Its a personal information program

---
+++ Info.java
@@ -0,0 +1,12 @@
+class me
+{
+public static void main(String args[])
+{
+System.out.println("Omkar Sandip Sutar");
+System.out.println("9552737133");
+System.out.println("22UAI119");
+System.out.println("CSE AI");
+System.out.println("Vivekanand nagar korochi");
+
+}
+}
