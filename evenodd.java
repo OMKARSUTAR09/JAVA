e1bd3353aae521b20719c82c50b942e27d9c4782
Omkar Sutar <159997804+OMKARSUTAR09@users.noreply.github.com>
2024-07-23 10:01:02 +0530

    Add files via upload
    Tells the number is even or odd

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+class evenodd{
+public static void main(String args[]){
+int i;
+int a[] = {2,5,6,3,4};
+for(i=0;i<=5;i++){
+if(a[i]%2==0)
+{
+System.out.println("even number");
+}
+else{
+System.out.println("odd number");
+}
+}
+}
+}
